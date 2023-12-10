3d525d619f6ff1e5e0fd9548e29ad9466352c5bc
Kuro-D-Shiro <iljashirobocov@gmail.com>
2023-12-10 22:50:51 +0400

    first commit

---
+++ BruhMobilApp/View/LogIn.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BruhMobilApp
+{
+    public partial class LogInPage : ContentPage
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+        private async void GoToTheRegistrationPage_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushModalAsync(new RegistrationPage());
+        }
+
+        private void LogInButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private async void PasswordRecovery_Clicked(object sender, EventArgs e)
+        {
+            //хз тут как-то логику посылания пароля нужно осуществить
+            await DisplayAlert("Уведомление.", "Ваш пароль пришёл вам на электронную почту.", "ОК");
+        }
+    }
+}
