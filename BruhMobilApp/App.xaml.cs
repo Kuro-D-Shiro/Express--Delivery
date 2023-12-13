91bf43f58d3e0ae240b503e69ceffbf0e85e993b
Kuro-D-Shiro <iljashirobocov@gmail.com>
2023-12-13 13:39:53 +0400

    добавил графический инетфейс для страницы выбора доставщиком заказа

--- BruhMobilApp/App.xaml.cs
+++ BruhMobilApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BruhMobilApp.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace BruhMobilApp
         {
             InitializeComponent();
 
-            MainPage = new LogInPage();
+            MainPage = new Deliveryman_OrderSelection();
         }
 
         protected override void OnStart()
