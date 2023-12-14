f32fbd8126f7eb412047920e023ccf7aa2871c60
Artur Sobolev <dragoogard@mail.ru>
2023-12-14 23:07:22 +0400

    Прописал тело для класса User

--- BruhMobilApp/Model/Domain.cs
+++ BruhMobilApp/Model/Domain.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace BruhMobilApp.Model
 {
-    class Class1
+    public class User
     {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class Deliverman : User
+    {
+
+    }
+    public class Customer : User
+    {
+
+    }
+    public class Package
+    {
+
     }
 }
