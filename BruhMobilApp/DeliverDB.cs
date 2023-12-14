5103a9ece256d0120bac1066d71314d2c9167df6
Egor Lozhkin <lozhkinyegor@yandex.ru>
2023-12-15 00:05:34 +0400

    Создал класс для работы с базой данных, основные методы работы с ней

---
+++ BruhMobilApp/DeliverDB.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace BruhMobilApp
+{
+    public class DeliverDB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=123456Йцукен;database=DeliverDB");
+
+        public void openConnection()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getConnection() {  return connection; }
+
+    }
+}
