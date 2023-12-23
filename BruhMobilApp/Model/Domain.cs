b45e5e5653233caa51450526038d845e0a5aaf9f
Egor Lozhkin <lozhkinyegor@yandex.ru>
2023-12-23 23:48:46 +0400

    Рефакторинг конструктора у  пользователя и посылки

--- BruhMobilApp/Model/Domain.cs
+++ BruhMobilApp/Model/Domain.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 
@@ -9,23 +11,47 @@ namespace BruhMobilApp.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Email {
+            get=>Email;
+            set
+            {
+                if (value.Contains("@"))
+                    Email = value;
+                else
+                    throw new Exception($"Wrong email {value}, must contains @");
+            }
+        }
+        public string Password {
+            get => Password;
+            set
+            {
+                if (value.All(char.IsDigit))
+                    Number = value;
+                else
+                    throw new Exception($"Wrong number {value}, must contains only digits");
+            }
+        }
         public string Number { get; set; }
-        public string Role { get; set; }
+        public string Role {
+            get => Role;
+            set
+            {
+                if (value == "deliverman" || value == "customer")
+                    Role = value;
+                else 
+                    throw new Exception($"Wrong role {value}, must be deliverman or customer");
+            }
+        }
         private List<Package> Packages { get; set; }
 
         public User(int id, string name, string email, string passowrd, string number, string role) 
         {
             Id = id;
             Name = name;
-            if (email.Contains("@")) Email = email;
-            else throw new Exception($"Wrong email {email}, must contains @");
+            Email = email;
             Password = passowrd;
-            if (number.All(char.IsDigit)) Number = number;
-            else throw new Exception($"Wrong number {number}, must contains only digits");
-            if (role == "deliverman" || role == "customer") Role = role;
-            else throw new Exception($"Wrong role {role}, must be deliverman or customer");
+            Number = number;
+            Role = role;
         }
 
         public User(int id, string name, string email, string passowrd, string number, string role, List<Package> packages):
@@ -37,13 +63,19 @@ namespace BruhMobilApp.Model
 
     public class Deliverman : User
     {        
-        public string Status { get; set; }
+        public string Status {
+            get => Status;
+            set
+            {
+                if (value == "busy" || value == "free") Status = value;
+                else throw new Exception($"Wrong status {value} must be busy or free");
+            }
+        }
 
         public Deliverman(int id, string name, string email, string passowrd, string number, string role, string status):
             base(id, name, email, passowrd, number, role)
         {
-            if (status == "busy" || status == "free") Status = status;
-            else throw new Exception($"Wrong status {status} must be busy or free");
+            Status = status;
         }
     }
     public class Customer : User
@@ -61,23 +93,60 @@ namespace BruhMobilApp.Model
     public class Package
     {
         public int Id { get; set; }
-        public string StartAddress { get; set; }
-        public string EndAddress { get; set; }
+        public string StartAddress {
+            get => StartAddress;
+            set
+            {
+                if (value.Length != 0)
+                    StartAddress = value;
+                else
+                    throw new Exception("Address cant be empty");
+            }
+        }
+        public string EndAddress {
+            get => EndAddress; 
+            set
+            {
+                if (value.Length != 0)
+                    EndAddress = value;
+                else
+                    throw new Exception("Addres cant be empty");
+            }
+        }
         public string Comment { get; set; }
-        public string Size {  get; set; }
+        public string Size {
+            get=>Size;
+            set
+            {
+                if (value == "small" || value == "standard" || value == "big")
+                    Size = value;
+                else
+                    throw new Exception($"Wrong size {value} must be small, standard or big");
+            }
+
+        }
         public DateTime Time { get; set; }
+        public double Cost { get; set; }
+        public string Status {  get; set; }
 
         public Package(int id, string startAddress, string endAddress, string comment, string size, DateTime time)
         {
             Id = id;
-            if (startAddress.Length != 0) StartAddress = startAddress;
-            else throw new Exception("Address cant be empty");
-            if (endAddress.Length != 0) EndAddress = endAddress;
-            else throw new Exception("Addres cant be empty");
+            StartAddress = startAddress;
+            EndAddress = endAddress;
             Comment = comment;
-            if (size == "small" || size == "standard" || size == "big") Size = size;
-            else throw new Exception($"Wrong size {size} must be small, standard or big");
+            Size = size;
             Time = time;
+        }
+
+        public double CalculateCost(int distanceInMeters)
+        {
+            var k = 1e-2;
+            var sizeFactor = new Dictionary<string, double>()
+            {{"small", 0.5 }, {"standard", 1.0 }, {"big", 2.0 }};
+
+            Cost = distanceInMeters * sizeFactor[Size];
+            return Cost;
         }
     }
 }
