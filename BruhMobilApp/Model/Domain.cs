cbd2c1c378ea155c085384733164180b05485f67
Egor Lozhkin <lozhkinyegor@yandex.ru>
2023-12-24 13:49:18 +0400

    Поля и свойства Domain исправил

--- BruhMobilApp/Model/Domain.cs
+++ BruhMobilApp/Model/Domain.cs
@@ -11,42 +11,47 @@ namespace BruhMobilApp.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email {
-            get=>Email;
+        public string Email
+        {
+            get => email;
             set
             {
                 if (value.Contains("@"))
-                    Email = value;
+                    email = value;
                 else
                     throw new Exception($"Wrong email {value}, must contains @");
             }
         }
-        public string Password {
-            get => Password;
+        public string Password { get; set; }
+        public string Number
+        {
+            get => number;
             set
             {
                 if (value.All(char.IsDigit))
-                    Number = value;
+                    number = value;
                 else
                     throw new Exception($"Wrong number {value}, must contains only digits");
             }
         }
-        public string Number { get; set; }
-        public string Role {
-            get => Role;
+        public string Role
+        {
+            get => role;
             set
             {
                 if (value == "deliverman" || value == "customer")
-                    Role = value;
-                else 
+                    role = value;
+                else
                     throw new Exception($"Wrong role {value}, must be deliverman or customer");
             }
         }
         private List<Package> Packages { get; set; }
+        private string email;
+        private string number;
+        private string role;
 
-        public User(int id, string name, string email, string passowrd, string number, string role) 
+        public User(string name, string email, string passowrd, string number, string role)
         {
-            Id = id;
             Name = name;
             Email = email;
             Password = passowrd;
@@ -54,26 +59,28 @@ namespace BruhMobilApp.Model
             Role = role;
         }
 
-        public User(int id, string name, string email, string passowrd, string number, string role, List<Package> packages):
-            this(id, name, email, passowrd, number, role)
+        public User(string name, string email, string passowrd, string number, string role, List<Package> packages) :
+            this(name, email, passowrd, number, role)
         {
             Packages = packages;
         }
     }
 
     public class Deliverman : User
-    {        
-        public string Status {
-            get => Status;
+    {
+        public string Status
+        {
+            get => status;
             set
             {
-                if (value == "busy" || value == "free") Status = value;
+                if (value == "busy" || value == "free") status = value;
                 else throw new Exception($"Wrong status {value} must be busy or free");
             }
         }
+        private string status;
 
-        public Deliverman(int id, string name, string email, string passowrd, string number, string role, string status):
-            base(id, name, email, passowrd, number, role)
+        public Deliverman(string name, string email, string passowrd, string number, string role, string status) :
+            base(name, email, passowrd, number, role)
         {
             Status = status;
         }
@@ -81,45 +88,48 @@ namespace BruhMobilApp.Model
     public class Customer : User
     {
         /* Просто дублирует конструкторы чтоб класс не пустой был*/
-        public Customer(int id, string name, string email, string passowrd, string number, string role) :
-            base(id, name, email, passowrd, number, role)
+        public Customer(string name, string email, string passowrd, string number, string role) :
+            base(name, email, passowrd, number, role)
         { }
-      
-        
-        public Customer(int id, string name, string email, string passowrd, string number, string role, List<Package> packages) :
-            base(id, name, email, passowrd, number, role, packages)
+
+
+        public Customer(string name, string email, string passowrd, string number, string role, List<Package> packages) :
+            base(name, email, passowrd, number, role, packages)
         { }
     }
     public class Package
     {
         public int Id { get; set; }
-        public string StartAddress {
-            get => StartAddress;
+        public string StartAddres
+        {
+            get => startAddres;
             set
             {
                 if (value.Length != 0)
-                    StartAddress = value;
+                    startAddres = value;
                 else
                     throw new Exception("Address cant be empty");
             }
         }
-        public string EndAddress {
-            get => EndAddress; 
+        public string EndAddres
+        {
+            get => endAddres;
             set
             {
                 if (value.Length != 0)
-                    EndAddress = value;
+                    endAddres = value;
                 else
                     throw new Exception("Addres cant be empty");
             }
         }
         public string Comment { get; set; }
-        public string Size {
-            get=>Size;
+        public string Size
+        {
+            get => size;
             set
             {
                 if (value == "small" || value == "standard" || value == "big")
-                    Size = value;
+                    size = value;
                 else
                     throw new Exception($"Wrong size {value} must be small, standard or big");
             }
@@ -127,24 +137,29 @@ namespace BruhMobilApp.Model
         }
         public DateTime Time { get; set; }
         public double Cost { get; set; }
-        public string Status{
-            get => Status;
+        public string Status
+        {
+            get => status;
             set
             {
                 var allowedStatus = new string[] { "wait", "delivered", "trasport", "rejected" };
                 if (allowedStatus.Contains(value))
-                    Status = value;
-                else 
+                    status = value;
+                else
                     throw new Exception($"{value} is not allowed, mast be" +
                         $" \"wait\", \"delivered\", \"trasport\", \"rejected\"");
             }
         }
 
-        public Package(int id, string startAddress, string endAddress, string comment, string size, DateTime time)
+        private string startAddres;
+        private string endAddres;
+        private string size;
+        private string status;
+
+        public Package(string startAddress, string endAddress, string comment, string size, DateTime time)
         {
-            Id = id;
-            StartAddress = startAddress;
-            EndAddress = endAddress;
+            StartAddres = startAddress;
+            EndAddres = endAddress;
             Comment = comment;
             Size = size;
             Time = time;
