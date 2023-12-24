a1073a2f0e02e7e67c564a75a9d9bf419b7ba0fc
Kuro-D-Shiro <iljashirobocov@gmail.com>
2023-12-24 13:53:11 +0400

    добавил страницу, которая открывается после того, как курьер доставил посылку

---
+++ BruhMobilApp/View/GoodJobPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BruhMobilApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GoodJobPage : ContentPage
+    {
+        public GoodJobPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
