05f44ef30dc2d0a1ef5d5d5555ef5cf9f2a78ddc
Kuro-D-Shiro <iljashirobocov@gmail.com>
2023-12-26 19:28:12 +0400

    добавил третий тест

--- ValidationTests/ValidTests.cs
+++ ValidationTests/ValidTests.cs
@@ -25,5 +25,43 @@ namespace ValidationTests
             e_Mail_Validation.OnEntryTextChange(entry, e);
             Debug.Assert(Color.Red == entry.TextColor);
         }
+
+        [TestMethod]
+        public void CorrectPhoneNumberTest1()
+        {
+            Entry entry = new Entry();
+            PhoneNumber_Validation phoneNumber_Validation = new PhoneNumber_Validation();
+            TextChangedEventArgs e = new TextChangedEventArgs(entry.Text, "(982) 791-21 02");
+            phoneNumber_Validation.OnEntryTextChange(entry, e);
+            Debug.Assert(Color.White == entry.TextColor);
+        }
+
+        [TestMethod]
+        public void CorrectPhoneNumberTest2()
+        {
+            Entry entry = new Entry();
+            PhoneNumber_Validation phoneNumber_Validation = new PhoneNumber_Validation();
+            TextChangedEventArgs e = new TextChangedEventArgs(entry.Text, "9827912102");
+            phoneNumber_Validation.OnEntryTextChange(entry, e);
+            Debug.Assert(Color.White == entry.TextColor);
+        }
+        [TestMethod]
+        public void InCorrectPhoneNumberTest1()
+        {
+            Entry entry = new Entry();
+            PhoneNumber_Validation phoneNumber_Validation = new PhoneNumber_Validation();
+            TextChangedEventArgs e = new TextChangedEventArgs(entry.Text, "");
+            phoneNumber_Validation.OnEntryTextChange(entry, e);
+            Debug.Assert(Color.Red == entry.TextColor);
+        }
+        [TestMethod]
+        public void InCorrectPhoneNumberTest2()
+        {
+            Entry entry = new Entry();
+            PhoneNumber_Validation phoneNumber_Validation = new PhoneNumber_Validation();
+            TextChangedEventArgs e = new TextChangedEventArgs(entry.Text, "sdfowefD2304we");
+            phoneNumber_Validation.OnEntryTextChange(entry, e);
+            Debug.Assert(Color.Red == entry.TextColor);
+        }
     }
 }
